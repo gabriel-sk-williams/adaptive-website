f230a3f17c84ef219c007f261a6c7cafcfe5717c
Gabriel Williams <gabrielskwilliams@gmail.com>
2021-08-17 15:37:56 -0400

    version fix

--- src/Adaptive.js
+++ src/Adaptive.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 //import { Link } from "react-router-dom";
-//import Button from './Button';
 import NavTop from './NavTop';
 import Footer from './Footer';
 import Landscape from './svg/landscape.svg';
